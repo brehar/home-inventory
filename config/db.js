201944b5524d654fdbd5bbc2505c252c49f1b9c5
Brett Hartman <brett.hartman@mac.com>
2016-04-27 09:32:10 -0700

    configure db for heroku

--- config/db.js
+++ config/db.js
@@ -2,19 +2,21 @@
 
 var mysql = require('mysql');
 
-var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'secret'
-});
+// var db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'secret'
+// });
+
+var db = mysql.createConnection(process.env.JAWSDB_URL);
 
 db.connect();
 
-db.query('CREATE DATABASE IF NOT EXISTS inventory', function(err) {
-    if (err) throw err;
-
-    db.query('USE inventory', function(err) {
-        if (err) throw err;
+// db.query('CREATE DATABASE IF NOT EXISTS inventory', function(err) {
+//     if (err) throw err;
+//
+//     db.query('USE inventory', function(err) {
+//         if (err) throw err;
 
         db.query('CREATE TABLE IF NOT EXISTS rooms (id INTEGER PRIMARY KEY AUTO_INCREMENT, name TEXT)', function(err) {
             if (err) throw err;
@@ -27,7 +29,7 @@ db.query('CREATE DATABASE IF NOT EXISTS inventory', function(err) {
                 });
             });
         });
-    });
-});
+//     });
+// });
 
 module.exports = db;
